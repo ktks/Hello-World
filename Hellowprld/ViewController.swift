1e0e1af7507aacc8a67ce8224987dc70826e9def
tsukasa.kimura <ktkszion@yahoo.co.jp>
2020-07-29 13:28:53 +0900

    Initial Commit

---
+++ Hellowprld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hellowprld
+//
+//  Created by 木村宰 on 2020/07/29.
+//  Copyright © 2020 Tsukasa Kimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
